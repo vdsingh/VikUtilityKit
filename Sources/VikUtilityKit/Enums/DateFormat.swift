da1d1959adffe0867552ffaf5dd5c3d868311119
Vikram Singh <vik.devmail@gmail.com>
2023-05-15 02:19:08 -0400

    C: Implement DateFormat, SystemIcon, Weekday, Date_Extension

---
+++ Sources/VikUtilityKit/Enums/DateFormat.swift
@@ -0,0 +1,14 @@
+//
+//  DateFormat.swift
+//
+//
+//  Created by Vikram Singh on 5/14/23.
+//
+
+import Foundation
+
+// TODO: Docstrings
+public enum DateFormat: String {
+    case standardTime = "h:mm a"
+    case fullDateWithTime = "MMM d, h:mm a"
+}
